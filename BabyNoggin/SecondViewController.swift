2153334c8686964a697603fb2ecb82f1600b32dd
Paras Dhawan <parasdhawan@Parass-MacBook-Pro.local>
2015-09-17 11:08:01 +0530

    Added the initial tabbed project swift template. Defaults to ios 8.4 for iphone 5s.

---
+++ BabyNoggin/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BabyNoggin
+//
+//  Created by Paras Dhawan on 17/09/15.
+//  Copyright (c) 2015 BabyNoggin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
